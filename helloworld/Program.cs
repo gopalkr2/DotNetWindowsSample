a73336a0f4b5ddf72c19b80476f73732cf8e2265
unknown <GOPALKR2@PHUSEH-L74503.NA.novartis.net>
2013-11-12 12:46:21 -0500

    check if number is prime

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -9,10 +9,66 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
-            Console.WriteLine("End");
+            Console.WriteLine("Lets check prime numbers, enter a number");
+            string num = Console.ReadLine();
+            int prime = 2;
+
+            if (int.TryParse(num, out prime))
+            {
+                Console.WriteLine("Sorry Error, try again");
+            }
+            else
+            {
+                if (IsPrime(prime))
+                {
+                    Console.WriteLine("The number is prime");
+                }
+                else
+                {
+
+                    Console.WriteLine("The number is not prime");
+                }
+            }
+            Console.ReadKey();
 
             //end of program
         }
+
+        private static bool IsPrime(int number)
+        {
+            //check for evenness
+            if (number % 2 == 0)
+            {
+                if (number == 2)
+                {
+                    return true;
+                }
+                return false;
+            }
+            //don't need to check past the square root
+            int max = (int)Math.Sqrt(number);
+            for (int i = 3; i <= max; i += 2)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+
+            while (true) { ;}
+
+
+            //repeat, Am I crazy ?
+            for (int i = 3; i <= max; i += 2)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
     }
 }
